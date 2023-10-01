2b5ee17512d3dd3b45822a821d3171da4a53054b
Md. Shoaib Akhter Shifat <sifuisu3@gmail.com>
2023-10-01 12:07:31 +0600

    navbar responsive

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { BiSearch } from "react-icons/bi";
+import { FaBars } from "react-icons/fa";
+import { AiFillCloseCircle } from "react-icons/ai";
 
 function Navbar() {
+  const [showNav, setShowNav] = useState(false);
   return (
     <div className="nav">
       <div className="navContainer">
-        <h1 className="logo">Grocery Store</h1>
-        <div className="navItems">
-          <div className="search">
-            <BiSearch className="searchIcon" />
-            <input type="text" className="searchInput" placeholder="Search" />
+        <div className="navWrapper">
+          <h1 className="logo">Grocery Store</h1>
+          <div className={`${showNav ? "navItems showNav" : "navItems"}`}>
+            <AiFillCloseCircle
+              className="navBtn navBtnClose"
+              onClick={() => setShowNav(!showNav)}
+            />
+            <div className="search">
+              <BiSearch className="searchIcon" />
+              <input type="text" className="searchInput" placeholder="Search" />
+            </div>
+            <span>Home</span>
+            <span>About</span>
+            <span>Contact Us</span>
           </div>
-          <span>Home</span>
-          <span>About</span>
-          <span>Contact Us</span>
         </div>
+        <FaBars className="navBtn" onClick={() => setShowNav(!showNav)} />
       </div>
     </div>
   );
