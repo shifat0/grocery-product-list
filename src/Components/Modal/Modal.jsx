4e5a952b81dc2f26e285aa8b3b230166d40bd87b
Md. Shoaib Akhter Shifat <sifuisu3@gmail.com>
2023-10-02 16:37:40 +0600

    product cart added to localstorage

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./modal.css";
 import { AiFillCloseCircle } from "react-icons/ai";
 
 function Modal({ showModal, modalToggle, product }) {
+  const [productCount, setProductCount] = useState(0);
+  let productObj = {
+    product: product,
+    orederCount: productCount,
+  };
+  let productArr = [];
+  const handleCart = () => {
+    const prevProduct = JSON.parse(localStorage.getItem("product-cart"));
+    prevProduct === null
+      ? productArr.push(productObj)
+      : productArr.push(...prevProduct, productObj);
+    localStorage.setItem("product-cart", JSON.stringify(productArr));
+  };
   return (
     <>
       {showModal && (
@@ -26,7 +39,32 @@ function Modal({ showModal, modalToggle, product }) {
               <img src={product.image} alt="product" />
             </div>
           </div>
-          <button className="cartBtn">Add to Cart</button>
+          <div className="countProduct">
+            <span
+              className={`${
+                productCount === 0 ? "modalCount disabled" : "modalCount"
+              }`}
+              onClick={() => setProductCount((count) => Math.max(count - 1, 0))}
+            >
+              -
+            </span>
+            <span className="counter">{productCount}</span>
+            <span
+              className="modalCount"
+              onClick={() => setProductCount(productCount + 1)}
+            >
+              +
+            </span>
+          </div>
+          <button
+            className="cartBtn"
+            onClick={() => {
+              handleCart();
+              modalToggle();
+            }}
+          >
+            Add to Cart
+          </button>
         </div>
       )}
     </>
