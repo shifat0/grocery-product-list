83304a01e3c714539acd9b9ae049ca23f72adb20
Md. Shoaib Akhter Shifat <sifuisu3@gmail.com>
2023-10-02 20:50:56 +0600

    page loading added

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -3,17 +3,23 @@ import "./products.css";
 import axios from "axios";
 import Modal from "../Modal/Modal";
 import { searchContext } from "../../Pages/Home";
+import { ImSpinner } from "react-icons/im";
 
 function Products() {
   const [products, setProducts] = useState([]);
   const [showModal, setShowModal] = useState(false);
   const [singleProduct, setSingleProduct] = useState({});
   const { searchText } = useContext(searchContext);
-  console.log(process.env.REACT_APP_API_URL);
+  const [loading, setLoading] = useState(false);
+  console.log(loading);
   useEffect(() => {
     const getProduct = async () => {
+      setLoading(true);
       const res = await axios.get(`${process.env.REACT_APP_API_URL}`);
-      setProducts(res?.data);
+      if (res) {
+        setProducts(res?.data);
+        setLoading(false);
+      }
     };
     getProduct();
   }, []);
@@ -21,32 +27,41 @@ function Products() {
 
   return (
     <>
-      <div className="productContainer">
-        {products
-          ?.filter((product) =>
-            searchText.toLowerCase() === ""
-              ? product
-              : product.name.toLowerCase().includes(searchText)
-          )
-          .map((product) => (
-            <div
-              key={product?._id}
-              className="productCard"
-              onClick={() => {
-                modalToggle();
-                setSingleProduct(product);
-              }}
-            >
-              <img src={product?.image} alt="product-img" />
-              <h2 className="productName">{product?.name}</h2>
-            </div>
-          ))}
-        <Modal
-          showModal={showModal}
-          modalToggle={() => modalToggle()}
-          product={singleProduct}
-        />
-      </div>
+      {loading ? (
+        <ImSpinner className="spinner" />
+      ) : (
+        <div className="productContainer">
+          {products
+            ?.filter((product) =>
+              searchText.toLowerCase() === ""
+                ? product
+                : product.name.toLowerCase().includes(searchText)
+            )
+            .map((product) => (
+              <div
+                key={product?._id}
+                className="productCard"
+                onClick={() => {
+                  modalToggle();
+                  setSingleProduct(product);
+                }}
+              >
+                <img
+                  src={product?.image}
+                  alt="product-img"
+                  className="productImg"
+                />
+
+                <h2 className="productName">{product?.name}</h2>
+              </div>
+            ))}
+          <Modal
+            showModal={showModal}
+            modalToggle={() => modalToggle()}
+            product={singleProduct}
+          />
+        </div>
+      )}
     </>
   );
 }
